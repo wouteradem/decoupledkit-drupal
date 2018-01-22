80e2e582ca21aaabbe78cb03eb04721a9958c3e5
Jason Enter <jason.enter@acquia.com>
2018-01-22 17:31:36 -0500

    BLT-000: Initial headless lightning build.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['decoupledkit.local'] = array(
+  'root' => '/var/www/decoupledkit/docroot',
+  'uri' => 'http://local.decoupledkit.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['decoupledkit.local'] += array(
+    'remote-host' => 'local.decoupledkit.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
